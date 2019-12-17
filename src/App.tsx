90ed48077a89a3daf19c864beb4339fc8301b19f
HauptmanDev <MainVados@gmail.com>
2019-12-17 22:38:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export type NotificationType = 'success' | 'warning' | 'error'
+
+interface IProps {
+
 }
 
-export default App;
+interface INotification {
+    id: Number,
+    title: String,
+    type: NotificationType
+}
+
+interface IState {
+    notifications: Array<INotification>
+}
+
+const Notification = (props: any) => {
+    return <div className={'notification' + props.className}>{props.text}</div>
+};
+
+class App extends Component <IProps, IState> {
+    state: IState = {
+        notifications: []
+    };
+
+    addNotification = (type: NotificationType, title: String) => {
+        const noti = {id: (new Date().getTime()), title: title, type: 'success' as NotificationType};
+        this.setState({
+            notifications: [...this.state.notifications, noti]
+        });
+        // setTimeout(()=> {
+        //     this.setState({
+        //
+        //     });
+        // },3000)
+    };
+
+    successClick = () => {
+        this.addNotification('success', 'Yes')
+    };
+
+    warningClick = () => {
+        this.addNotification('warning', 'Mmm')
+    };
+
+    errorClick = () => {
+        this.addNotification('error', 'Nooo!')
+    };
+
+    render() {
+        let element = this.state.notifications.map(n => {
+            return <Notification key={n.id} text={n.title} className={n.type}/>
+        });
+        return (
+            <div className='App'>
+                <button onClick={this.successClick}>Success</button>
+                <button onClick={this.warningClick}>Warning</button>
+                <button onClick={this.errorClick}>Error</button>
+                <div className='notification-block'>
+                    {element}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
